e9db50fd8de12c70a1e62ef9e9bebefb58c5f24d
leerkz <l3ra131007@gmail.com>
2024-09-23 17:47:49 +0300

    done hw14.2

--- main.py
+++ main.py
@@ -1,73 +1,52 @@
 class Product:
-    name: str
-    description: str
-    price: float
-    quantity: int
-
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, value: float) -> None:
+        if value <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = value
+
+    @classmethod
+    def new_product(cls, product_data: dict) -> "Product":
+        return cls(
+            name=product_data.get("name", "None"),
+            description=product_data.get("description", "None"),
+            price=product_data["price"],
+            quantity=product_data.get("quantity", 0),
+        )
 
 
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name: str, description: str, products: list | None = None):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products if products else []
 
-        Category.product_count += len(self.products)
+        Category.product_count += len(self.__products)
         Category.category_count += 1
 
+    def add_product(self, product: Product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
 
-# if __name__ == "__main__":
-#     product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
-#     product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-#     product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-#
-#     print(product1.name)
-#     print(product1.description)
-#     print(product1.price)
-#     print(product1.quantity)
-#
-#     print(product2.name)
-#     print(product2.description)
-#     print(product2.price)
-#     print(product2.quantity)
-#
-#     print(product3.name)
-#     print(product3.description)
-#     print(product3.price)
-#     print(product3.quantity)
-#
-#     category1 = Category("Смартфоны",
-#                          "Смартфоны, как средство не только коммуникации,
-#                          но и получения дополнительных функций для удобства жизни",
-#                          [product1, product2, product3])
-#
-#     print(category1.name == "Смартфоны")
-#     print(category1.description)
-#     print(len(category1.products))
-#     print(category1.category_count)
-#     print(category1.product_count)
-#
-#     product4 = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-#     category2 = Category("Телевизоры",
-#                          "Современный телевизор, который позволяет наслаждаться просмотром,
-#                          станет вашим другом и помощником",
-#                          [product4])
-#
-#     print(category2.name)
-#     print(category2.description)
-#     print(len(category2.products))
-#     print(category2.products)
-#
-#     print(Category.category_count)
-#     print(Category.product_count)
+    @property
+    def products(self) -> str:
+        return "\n".join(
+            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт." for product in self.__products]
+        )
